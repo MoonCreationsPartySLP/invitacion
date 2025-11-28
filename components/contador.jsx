d06317cde9a81398f6301e9255eccf8bb842fb17
Moon Creations Party SLP <mooncreationsparty@gmail.com>
2025-11-27 20:37:58 -0600

    Update contador.jsx

--- components/contador.jsx
+++ components/contador.jsx
@@ -58,7 +58,7 @@ return (
     </div>
 
     {/* Subtítulo */}
-    <div className="mt-6 text-stone-600 tracking-[0.3em] font-parisienne">
+    <div className="mt-6 text-stone-600 tracking-[0.3em] font-antic">
       FALTAN
     </div>
 
